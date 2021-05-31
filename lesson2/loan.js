d2b675ee76f11612adc9f45c8ef03014c39acb9e
Jay Kasiphat <kasiphatpir@gmail.com>
2021-06-01 00:52:02 +0700

    Check for invalid input

--- lesson2/loan.js
+++ lesson2/loan.js
@@ -5,6 +5,10 @@ function toDecimal(percent) {
   return percent / 100;
 }
 
+function invalidInput(input) {
+  return input.trim() === '' || Number.isNaN(Number(input));
+}
+
 function calculateMonthlyInterest(apr) {
   return apr / 12;
 }
@@ -16,20 +20,36 @@ function calculateMortgage(loanAmount, apr, loanDurationInMonths) {
     (1 - Math.pow((1 + monthlyInterest), (-loanDurationInMonths))));
   return mortgate.toFixed(2);
 }
-
+ 
 console.log('Welcome to Mortgage Calculator!');
 
 console.log('What is your loan amount? EX: $1200');
 let loanAmount = RL_SYNC.question('$');
-loanAmount = Number(loanAmount);
+
+while (invalidInput(loanAmount)) {
+  console.log('Please enter numbers only.');
+  loanAmount = RL_SYNC.question('$');
+}
 
 console.log('What is the APR? EX: %8');
 let apr = RL_SYNC.question('%');
-apr = Number(apr);
+
+while (invalidInput(apr)) {
+  console.log('Please enter numbers only.');
+  apr = RL_SYNC.question('$');
+}
 
 console.log('What is the loan duration (months)? EX: 24');
 let loanDuration = RL_SYNC.question();
-loanDuration = Number(loanDuration);
 
+while (invalidInput(loanDuration)) {
+  console.log('Please enter numbers only.');
+  loanDuration = RL_SYNC.question('$');
+}
+
+loanAmount = Number(loanAmount);
+apr = Number(apr);
+loanDuration = Number(loanDuration);
 let monthlyPayment = calculateMortgage(loanAmount, apr, loanDuration);
+
 console.log(`$${monthlyPayment}`);
