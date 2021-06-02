bbaa5080d17a78954a29eeac0ef282bf46792ca2
Jay Kasiphat <kasiphatpir@gmail.com>
2021-06-02 22:32:01 +0700

    Remove trailing spaces

--- small_problems/01easy1/ex05.js
+++ small_problems/01easy1/ex05.js
@@ -1,7 +1,7 @@
 /*
-Create a simple tip calculator. The program should prompt for a bill amount 
-and a tip rate. The program must compute the tip, and then log both the tip 
-and the total amount of the bill to the console. You can ignore input 
+Create a simple tip calculator. The program should prompt for a bill amount
+and a tip rate. The program must compute the tip, and then log both the tip
+and the total amount of the bill to the console. You can ignore input
 validation and assume that the user will enter numbers.
 */
 
