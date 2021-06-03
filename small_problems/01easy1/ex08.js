a395c8f16f883b59a037f24263d77f8df26bb5c6
Jay Kasiphat <kasiphatpir@gmail.com>
2021-06-03 20:21:49 +0700

    Remove lines and spaces

--- small_problems/01easy1/ex08.js
+++ small_problems/01easy1/ex08.js
@@ -1,6 +1,6 @@
 /*
-In the modern era under the Gregorian Calendar, leap years occur in 
-every year that is evenly divisible by 4, unless the year is also 
+In the modern era under the Gregorian Calendar, leap years occur in
+every year that is evenly divisible by 4, unless the year is also
 divisible by 100. If the year is evenly divisible by 100, then it is
 not a leap year, unless the year is also evenly divisible by 400.
 
@@ -17,12 +17,10 @@ console.log(isLeapYear(2016));      // true
 console.log(isLeapYear(2015));      // false
 console.log(isLeapYear(2100));      // false
 console.log(isLeapYear(2400));      // true
-
 console.log(isLeapYear(240000));    // true
 console.log(isLeapYear(240001));    // false
 console.log(isLeapYear(2000));      // true
 console.log(isLeapYear(1900));      // false
-
 console.log(isLeapYear(1752));      // true
 console.log(isLeapYear(1700));      // false
 console.log(isLeapYear(1));         // false
