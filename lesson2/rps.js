2057bf4941b41fc7795b456a04b5841df9920a5a
Jay Kasiphat <kasiphatpir@gmail.com>
2021-06-04 23:40:54 +0700

    Add rps game

---
+++ lesson2/rps.js
@@ -0,0 +1,50 @@
+// ask the user for their move
+// the computer will choose their move
+// display who won/the result
+
+const readline = require('readline-sync');
+const VALID_CHOICES = ['rock', 'paper', 'scissors'];
+
+function prompt(msg) {
+  console.log(`=> ${msg}`);
+}
+
+function displayWinner(choice, computerChoice) {
+  prompt(`You chose ${choice}. Computer chose ${computerChoice}.`);
+
+  if ((choice === 'rock' && computerChoice === 'scissors') ||
+      (choice === 'paper' && computerChoice === 'rock') ||
+      (choice === 'scissors' && computerChoice === 'paper')) {
+    prompt('You won!');
+  } else if ((choice === 'rock' && computerChoice === 'paper') ||
+             (choice === 'paper' && computerChoice === 'scissors') ||
+             (choice === 'scissors' && computerChoice === 'rock')) {
+    prompt('Computer won!');
+  } else {
+    prompt("It's a tie!");
+  }
+}
+
+while (true) {
+  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+  let choice = readline.question();
+
+  while (!VALID_CHOICES.includes(choice)) {
+    prompt('Not a valid choice.');
+    choice = readline.question();
+  }
+
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[randomIndex];
+
+  displayWinner(choice, computerChoice);
+
+  prompt('Would you like to play again? (y/n)');
+  let answer = readline.question().toLowerCase();
+  while (answer[0] !== 'y' && answer[0] !== 'n') {
+    prompt("Please enter 'y' or 'n'");
+    answer = readline.question().toLowerCase();
+  }
+
+  if (answer[0] === 'n') break;
+}
