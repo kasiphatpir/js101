d36d6f14d9b339bf4b5cb9672b75bedfcc3026b4
Jay Kasiphat <kasiphatpir@gmail.com>
2021-06-13 21:00:54 +0700

    Solve question 7

---
+++ lesson3/easy2/q07.js
@@ -0,0 +1,8 @@
+/*
+Create an array from this object that contains only two elements:
+Barney's name and Barney's number: [ 'Barney', 2 ]
+*/
+
+let flintstones = { Fred: 0, Wilma: 1, Barney: 2, Betty: 3, Bambam: 4, Pebbles: 5 };
+let barney = Object.entries(flintstones).filter(pair => pair[0] === 'Barney').shift();
+console.log(barney);
